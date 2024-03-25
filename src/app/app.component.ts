f273f9032be6e2093195130f214492a67324c33a
bmaxtar <bmaxtar@gmail.com>
2024-03-25 20:31:02 +0000

    add product, ngIf method, ngFor,faker for data, ngOnit and onClick data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { createProduct, createProducts } from './data/product.data';
+import { SimpleProduct } from './interfaces/product';
 
 @Component({
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ng-edacy-training';
+export class AppComponent implements OnInit {
+  public products!: SimpleProduct[];
+  public product: SimpleProduct = createProduct();
+
+  constructor() {}
+
+  ngOnInit() {
+    this.products = createProducts(16);
+  }
+
+  isPromo(produit: SimpleProduct): boolean {
+    return produit.promo;
+  }
+
+  addToCart(ev: any) {
+    ev.preventDefault();
+    console.log('add to cart');
+    alert('Votre article a bien été ajouté au panier');
+  }
 }
---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,10 @@
+export interface SimpleProduct {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  price?: string;
+  category?: string;
+  promo: boolean;
+  promoVal?: number;
+}
